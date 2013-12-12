17011d23817433ab7866176e489372ab99bc4bf8
Joao Neto <joaomlneto@ist.utl.pt>
2013-12-12 18:25:42 +0100

    probes added (section 1.7)

--- chordy/node1.erl
+++ chordy/node1.erl
@@ -71,6 +71,18 @@ node(MyKey, Predecessor, Successor) ->
 		% stabilize ourselves periodically
 		stabilize ->
 			stabilize(Successor),
+			node(MyKey, Predecessor, Successor);
+		% received a probe from a client - make it go around!
+		probe ->
+			create_probe(MyKey, Successor),
+			node(MyKey, Predecessor, Successor);
+		% received the probe i sent back - it went around!
+		{probe, MyKey, Nodes, T} ->
+			remove_probe(MyKey, Nodes, T),
+			node(MyKey, Predecessor, Successor);
+		% receive a probe originating from someone else
+		{probe, RefKey, Nodes, T} ->
+			forward_probe(MyKey, RefKey, [MyKey|Nodes], T, Successor),
 			node(MyKey, Predecessor, Successor)
 end.
 
@@ -150,4 +162,28 @@ notify({Nkey, Npid}, MyKey, Predecessor) ->
 					Predecessor
 			end
 	end.
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% Probing-related functionality %%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% create_probe
+% we received a request for probing a DHT from a client
+% create a probe and make it go around the DHT - send to successor
+create_probe(MyKey, {Skey, Spid}) ->
+	Spid ! {probe, MyKey, [MyKey], erlang:now()},
+	io:format("[~w] Create probe ~w! Passing to ~w~n", [MyKey, MyKey, Skey]).
+
+% remove_probe
+% the probe we created went around! remove it from the DHT
+remove_probe(MyKey, Nodes, T) ->
+	Time = timer:now_diff(erlang:now(), T),
+	io:format("[~w] Received probe ~w in ~w ms Ring: ~w~n", [MyKey, MyKey, Time, Nodes]).
+
+% forward_probe
+% forward a probe originated in some other node along the DHT
+% Note: we have already been added to the Nodes list in the main loop (node/3)
+forward_probe(MyKey, RefKey, Nodes, T, {Skey, Spid}) ->
+	Spid ! {probe, RefKey, Nodes, T},
+	io:format("[~w] Forward probe ~w to ~w!~n", [MyKey, RefKey, Skey]).
+
 
